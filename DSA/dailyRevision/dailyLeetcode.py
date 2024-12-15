0c0b3a81c81c4b782d67eddb06477829cb359171
deepakrana@123 <rdev@69146@gmail.com>
2024-12-15 11:37:19 +0530

    a

--- DSA/dailyRevision/dailyLeetcode.py
+++ DSA/dailyRevision/dailyLeetcode.py
@@ -108,59 +108,92 @@ class Solution:
         return maxResult
 #     
 a=Solution(3,[4,5,8,2,3,5,10])
+# def __init__(self):
+#         self.minHeap=[]
+#         self.maxHeap=[]
+        
+#  # def addNum(self, num: int) -> None:
+#         heappush(self.maxHeap,-num)
+#         if self.maxHeap and self.minHeap and (-self.maxHeap[0]>self.minHeap[0]):
+#             heappush(self.minHeap,-heappop(self.maxHeap))
+#         if len(self.minHeap)+1<len(self.maxHeap):
+#             heappush(self.minHeap,-heappop(self.maxHeap))
+#         else:
+#             heappush(self.maxHeap,-heappop(self.minHeap))
+
+
+#     # def findMedian(self) -> float:
+#     #     if len(self.maxHeap) > len(self.minHeap):
+#     #         return -self.maxHeap[0]
+#     #     else:
+#     #         return (-self.maxHeap[0] + self.minHeap[0]) / 2.0
+#     # def totalCost(self, costs, k, candidates):
+#         hired=0
+#         ans=0
+#         queue1=[]
+#         queue2=[]
+#         i=0
+#         j=len(costs)-1
+#         while hired<k:
+#             while queue1 and i<=j:
+#                 heappush(queue1,costs[i])
+#                 i+=1
+#             while queue2 and j>=i:
+#                 heappush(queue2,costs[j])
+#                 j-=1
+#             a=queue1[-1] if queue1 else float("inf")
+#             b=queue2[-1] if queue2 else float("inf")
+#             if a<b:
+#                 ans+=a
+#                 heappop(queue1)
+#             else:
+#                 ans+=b
+#                 heappop(queue2)
+#             hired+=1
+#         return ans
+    
+
 # print(a.kSmallestPairs([1,1,2], nums2 = [1,2,3], k = 2))
 # print(a.frequencySort("tree"))
 # print(a.frequencySort('cccaaa'))
 # print(a.maximumLength("aaaa"))
 # print(a.maximumLength("aaaa"))
 # print(a.maximumBeauty([4,6,1,2],2))
+import heapq
 class MedianFinder:
-
-    def __init__(self):
-        self.minHeap=[]
-        self.maxHeap=[]
-        
-
-    def addNum(self, num: int) -> None:
-        heappush(self.maxHeap,-num)
-        if self.maxHeap and self.minHeap and (-self.maxHeap[0]>self.minHeap[0]):
-            heappush(self.minHeap,-heappop(self.maxHeap))
-        if len(self.minHeap)+1<len(self.maxHeap):
-            heappush(self.minHeap,-heappop(self.maxHeap))
-        else:
-            heappush(self.maxHeap,-heappop(self.minHeap))
-
-
-    # def findMedian(self) -> float:
-    #     if len(self.maxHeap) > len(self.minHeap):
-    #         return -self.maxHeap[0]
-    #     else:
-    #         return (-self.maxHeap[0] + self.minHeap[0]) / 2.0
-    def totalCost(self, costs, k, candidates):
-        hired=0
-        ans=0
-        queue1=[]
-        queue2=[]
-        i=0
-        j=len(costs)-1
-        while hired<k:
-            while queue1 and i<=j:
-                heappush(queue1,costs[i])
-                i+=1
-            while queue2 and j>=i:
-                heappush(queue2,costs[j])
-                j-=1
-            
-            a=queue1[-1] if queue1 else float("inf")
-            b=queue2[-1] if queue2 else float("inf")
-            if a<b:
-                ans+=a
-                heappop(queue1)
-            else:
-                ans+=b
-                heappop(queue2)
-            hired+=1
-        return ans
-            
+    # def maxAverageRatio(self, classes, extraStudents):
+    #     heap=[]
+    #     for i in range(len(classes)):
+    #         heap.append([classes[i][0]/classes[i][1],classes[i][0],classes[i][1]])
+    #     k=extraStudents
+    #     while heap and k:
+    #         a,b,c=heappop(heap)
+    #         heappush(heap,[(b+1)/(c+1),b+1,c+1])
+    #         k-=1
+    #     result=0
+    #     while heap:
+    #         a,b,c=heappop(heap)
+    #         result+=a
+    #     return result/len(classes)
+     def findScore(self, nums):
+        mp=set()
+        heap=[]
+        result=0
+        for i in range(len(nums)):
+            heap.append([nums[i],i])
+        heapq.heapify(heap)
+        while heap:
+            a,b=heappop(heap)
+            if b not in mp:
+                result+=a
+                mp.add(b)
+                if b-1>=0:
+                    mp.add(b-1)
+                if b+1<len(nums):
+                    mp.add(b+1)
+        return result
 
         
+abc=MedianFinder()
+print(abc.findScore([2,1,3,4,5,2]))
+# print(abc.maxAverageRatio( [[2,4],[3,9],[4,5],[2,10]], extraStudents = 4))
