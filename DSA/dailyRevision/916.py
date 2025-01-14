dfbe222e59ffcebb12a8f2f4b9d5dbf1e2ff06ac
deepakrana@123 <rdev@69146@gmail.com>
2025-01-14 22:38:54 +0530

    a

--- DSA/dailyRevision/916.py
+++ DSA/dailyRevision/916.py
@@ -34,7 +34,6 @@ def canConstruct(s, k):
 def findThePrefixCommonArray(A, B):
     abc=[0]*len(A)
     count=0
-    
     dicts={}
     for i in range(len(A)):
         dicts[B[i]]=dicts.get(B[i],0)+1
@@ -49,7 +48,6 @@ def findThePrefixCommonArray(A, B):
         abc[i]=count
     return abc
 def minimumLength(s):
-    n=len(s)
     dicts={}
     for i in range(len(s)):
         dicts[s[i]]=dicts.get(s[i],0)+1
@@ -59,13 +57,71 @@ def minimumLength(s):
     for key in dicts:
         count+=dicts[key]
     return count
+def canBeValid(s, locked):
+    open=[]
+    openClose=[]
+    i=0
+    if len(s)%2!=0:
+        return False
+    while i <len(s):
+        if locked[i]=='0':
+            openClose.append(i)
+        if locked[i]=='1':
+            if s[i]=='(':
+                open.append(i)
+            elif s[i]==')':
+                if open:
+                    open.pop()
+                elif openClose:
+                    openClose.pop()
+                else:
+                    return False
+        i+=1
+    while open and openClose and open[-1]<openClose[-1]:
+        open.pop()
+        openClose.pop()
+    return len(open)==0
+def isValid(mid,a,n):
+    queue=[]
+    visited=[False]*n
+    queue.append(0)
+    visited[0]=True
+    while queue:
+        u=queue.pop()
+        for v,w in a[u]:
+            if w != mid and visited[v]==False:
+                visited[v]=True
+                queue.append(v)
+    print(visited,mid)
+    for v in visited:
+        if v==False:
+            return False
+    return True
+def minMaxWeight(n, edges, threshold):
+    a={i:[] for i in range(n)}
+    minWeight=0
+    maxWeight=float('-inf')
+    result=float('inf')
+    for u,v,w in edges:
+        maxWeight = max(w,maxWeight)
+        a[v].append((u,w))
+    while minWeight<= maxWeight:
+        mid = minWeight + (maxWeight - minWeight)//2
+        if isValid(mid,a,n):
+            result = mid
+            maxWeight = mid-1
+        else:
+            minWeight = mid+1
+    return result if result!=float('inf') else -1
+
         
-
-
+    
         
 # print(canConstruct("true", k = 4))
 # print(wordSubsets(["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"]))
 # print(wordSubsets(["amazon","apple","facebook","google","leetcode"], words2 = ["lc","eo"]))
 # print(findThePrefixCommonArray([2,3,1], B = [3,1,2]))
 # print(findThePrefixCommonArray([1,3,2,4], B = [3,1,2,4]))
-print(minimumLength("abaacbcbb"))
+# print(minimumLength("abaacbcbb"))
+# print(canBeValid("))()))", locked = "010100"))
+print(minMaxWeight( 5, edges = [[1,0,1],[2,0,2],[3,0,1],[4,3,1],[2,1,1]], threshold = 2))
